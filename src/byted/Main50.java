baa40ddd1cdd550c4688247686e4c06201cd5922
yaozy <yaozy@tuya.com>
2020-07-14 16:52:14 +0800

    mac 5

--- src/byted/Main50.java
+++ src/byted/Main50.java
@@ -1,8 +1,38 @@
 package byted;
 
 /**
- * 1201 丑数3
+ * 162 峰值元素是指其值大于左右相邻值的元素。
+ *
+ * 给定一个输入数组 nums，其中 nums[i] ≠ nums[i+1]，找到峰值元素并返回其索引。
+ *
+ * 数组可能包含多个峰值，在这种情况下，返回任何一个峰值所在位置即可。
+ *
+ * 你可以假设 nums[-1] = nums[n] = -∞。
  */
 public class Main50 {
+    public static void main(String[] args) {
+        int n = findPeakElement(new int[]{2});
+        System.out.println(n);
+    }
+    public static int findPeakElement(int[] nums) {
+        if (nums==null || nums.length==0 ){
+            return -1;
+        }
+        if (nums.length==1){
+            return 0;
+        }
+        if (nums[0]>nums[1]){
+            return 0;
+        }
+        int n=nums.length;
+        if (nums[n-1]>nums[n-2]){
+            return n-1;
+        }
+        for (int i=1;i<n-1;i++){
+            if (nums[i]>nums[i-1] && nums[i]>nums[i+1])
+                return i;
+        }
+        return -1;
+    }
 
 }
---
+++ src/byted/Main51.java
@@ -0,0 +1,12 @@
+package byted;
+
+/**
+ * 4 给定两个大小为 m 和 n 的正序（从小到大）数组 nums1 和 nums2。
+ *
+ * 请你找出这两个正序数组的中位数，并且要求算法的时间复杂度为 O(log(m + n))。
+ *
+ * 你可以假设 nums1 和 nums2 不会同时为空。
+ */
+public class Main51 {
+
+}
